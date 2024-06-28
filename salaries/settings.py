cc7e328cd28adaedf464278807d9e17bf96b1890
Mohamed Khalef <medyacoubkhalef@gmail.com>
2024-06-28 11:06:49 +0000

    Adding whitenoise

--- salaries/settings.py
+++ salaries/settings.py
@@ -102,6 +102,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -144,7 +145,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse('postgresql://salaries_bd_user:D2rhJZra3HXqNw007ZTn2dUg5ncpcHSS@dpg-cpupg6qj1k6c738fbri0-a.oregon-postgres.render.com/salaries_bd')
+DATABASES['default'] = dj_database_url.parse('postgresql://salaries_bd_user:D2rhJZra3HXqNw007ZTn2dUg5ncpcHSS@dpg-cpupg6qj1k6c738fbri0-a/salaries_bd')
 # salaries_bd_user
 # D2rhJZra3HXqNw007ZTn2dUg5ncpcHSS
 # postgresql://salaries_bd_user:D2rhJZra3HXqNw007ZTn2dUg5ncpcHSS@dpg-cpupg6qj1k6c738fbri0-a/salaries_bd
